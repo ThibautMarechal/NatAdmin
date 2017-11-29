49e9e64f774876b44cd6b37d26f6d82e80f180fd
Quentin Grignet <quentin.grignet@skynet.be>
2017-11-29 10:51:46 +0100

    Test Ask

--- src/main/java/com/helmo/NatAdmin/services/BirdService.java
+++ src/main/java/com/helmo/NatAdmin/services/BirdService.java
@@ -2,49 +2,51 @@ package com.helmo.NatAdmin.services;
 
 import com.helmo.NatAdmin.models.Bird;
 import com.helmo.NatAdmin.services.crudServices.ICrudService;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BirdService implements ICrudService<Bird> {
-    @Override
-    public void create(Bird toCreate) {
-        //TODO REST CALL
-    }
-
-    @Override
-    public void delete(Bird toDelete) {
-        //TODO REST CALL
-    }
-
-    @Override
-    public void delete(long idToDelete) {
-        //TODO REST CALL
-    }
-
-    @Override
-    public List<Bird> getAll() {
-        //TODO REST CALL
-        List<Bird> birds = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            birds.add(getById(i));
-        }
-        return birds;
-    }
-
-    @Override
-    public Bird getById(long id) {
-        //TODO REST CALL
-        Bird b = new Bird();
-        b.setId(id);
-        b.setName("Bird #"+id);
-        b.setDescription("Bird Description #"+id);
-        b.setAverageHeigth((int) id);
-        return b;
-    }
-
-    @Override
-    public void update(Bird toUpdate) {
-        //TODO REST CALL
-    }
+	@Override
+	public void create(Bird toCreate) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public void delete(Bird toDelete) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public void delete(long idToDelete) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public List<Bird> getAll() {
+		//TODO REST CALL
+		List<Bird> birds = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			birds.add(getById(i));
+		}
+		
+		return birds;
+	}
+	
+	@Override
+	public Bird getById(long id) {
+		//TODO REST CALL
+		Bird b = new Bird();
+		b.setId(id);
+		b.setName("Bird #" + id);
+		b.setDescription("Bird Description #" + id);
+		b.setAverageHeigth((int) id);
+		return b;
+	}
+	
+	@Override
+	public void update(Bird toUpdate) {
+		//TODO REST CALL
+	}
 }
--- src/main/java/com/helmo/NatAdmin/services/UserService.java
+++ src/main/java/com/helmo/NatAdmin/services/UserService.java
@@ -2,55 +2,58 @@ package com.helmo.NatAdmin.services;
 
 import com.helmo.NatAdmin.models.User;
 import com.helmo.NatAdmin.services.crudServices.ICrudService;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserService implements ICrudService<User>
-{
-    public UserService(){}
-    @Override
-    public List<User> getAll()
-    {
-        //TODO REST CALL
-        List<User> users = new ArrayList<User>();
-        for (int i =1; i <= 10; ++i){
-            users.add(getById(i));
-        }
-        return users;
-    }
-
-    @Override
-    public User getById(long id)
-    {
-        //TODO REST CALL
-        User u = new User();
-        u.setFullName("fullname #"+id);
-        u.setEmail("email #"+id);
-        u.setId(id);
-        return u;
-    }
-
-    @Override
-    public void update(User toUpdate)
-    {
-        //TODO REST CALL
-    }
-
-    @Override
-    public void create(User toUpdate)
-    {
-        //TODO REST CALL
-    }
-
-    @Override
-    public void delete(User toUpdate)
-    {
-        //TODO REST CALL
-    }
-
-    @Override
-    public void delete(long idToDelete) {
-        //TODO REST CALL
-    }
+public class UserService implements ICrudService<User> {
+	public UserService() {
+	}
+	
+	@Override
+	public List<User> getAll() {
+		//TODO REST CALL
+		List<User> users = new ArrayList<User>();
+		for (int i = 1; i <= 10; ++i) {
+			users.add(getById(i));
+		}
+		final String uri = "http://192.168.128.13:8081/GRIMAR%2D1.0/users";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
+		System.out.print(result);
+		
+		return users;
+	}
+	
+	@Override
+	public User getById(long id) {
+		//TODO REST CALL
+		User u = new User();
+		u.setFullName("fullname #" + id);
+		u.setEmail("email #" + id);
+		u.setId(id);
+		return u;
+	}
+	
+	@Override
+	public void update(User toUpdate) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public void create(User toUpdate) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public void delete(User toUpdate) {
+		//TODO REST CALL
+	}
+	
+	@Override
+	public void delete(long idToDelete) {
+		//TODO REST CALL
+	}
 }
