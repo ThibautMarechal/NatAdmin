a164440fd0ad6f94ccf934dc95d73fc655746628
Thibaut Maréchal <contact@thibautmarechal.be>
2017-12-04 14:33:53 +0100

    fix add user

--- src/main/java/com/helmo/NatAdmin/models/Observation.java
+++ src/main/java/com/helmo/NatAdmin/models/Observation.java
@@ -1,7 +1,12 @@
 package com.helmo.NatAdmin.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Observation  extends Identifiable{
     private Date date;
     private String latitude;
@@ -11,84 +16,4 @@ public class Observation  extends Identifiable{
     private Bird bird;
     private String mediaType;
     private String mediaPath;
-
-    public Date getDate()
-    {
-        return date;
-    }
-
-    public void setDate(Date date)
-    {
-        this.date = date;
-    }
-
-    public String getLatitude()
-    {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude)
-    {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude()
-    {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude)
-    {
-        this.longitude = longitude;
-    }
-
-    public int getNumberOfBird()
-    {
-        return numberOfBird;
-    }
-
-    public void setNumberOfBird(int numberOfBird)
-    {
-        this.numberOfBird = numberOfBird;
-    }
-
-    public boolean isValid()
-    {
-        return isValid;
-    }
-
-    public void setValid(boolean valid)
-    {
-        isValid = valid;
-    }
-
-    public Bird getBird()
-    {
-        return bird;
-    }
-
-    public void setBird(Bird bird)
-    {
-        this.bird = bird;
-    }
-
-    public String getMediaType()
-    {
-        return mediaType;
-    }
-
-    public void setMediaType(String mediaType)
-    {
-        this.mediaType = mediaType;
-    }
-
-    public String getMediaPath()
-    {
-        return mediaPath;
-    }
-
-    public void setMediaPath(String mediaPath)
-    {
-        this.mediaPath = mediaPath;
-    }
 }
--- src/main/java/com/helmo/NatAdmin/models/User.java
+++ src/main/java/com/helmo/NatAdmin/models/User.java
@@ -11,46 +11,4 @@ public class User extends Identifiable
     private String email;
     private boolean isAdmin;
     private String profilePictureUrl;
-    public User()
-    {
-    }
-/*
-    public String getFullName()
-    {
-        return fullName;
-    }
-
-    public void setFullName(String fullName)
-    {
-        this.fullName = fullName;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public boolean isAdmin()
-    {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin)
-    {
-        isAdmin = admin;
-    }
-
-    public String getProfilePictureUrl() {
-        return profilePictureUrl;
-    }
-
-    public void setProfilePictureUrl(String profilePictureUrl) {
-        this.profilePictureUrl = profilePictureUrl;
-    }
-    */
 }
