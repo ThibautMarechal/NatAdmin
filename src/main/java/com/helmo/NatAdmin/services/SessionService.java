37341267780dffdfc4bce0b104ab1b0cd3e2ef6a
Quentin Grignet <quentin.grignet@skynet.be>
2017-12-05 11:07:37 +0100

    Define Sessions Service

--- src/main/java/com/helmo/NatAdmin/services/SessionService.java
+++ src/main/java/com/helmo/NatAdmin/services/SessionService.java
@@ -23,47 +23,52 @@ public class SessionService implements ICrudService<Session> {
 	public SessionService(RestTemplate template, CallREST caller) {
 		restTemplate = template;
 		this.caller = caller;
+		restTemplate.getInterceptors().add(
+			  new BasicAuthorizationInterceptor("admin@nat.be", "adminadmin")
+		);
 	}
 	
 	@Override
 	public long create(Session toCreate) {
-		//TODO
-		return 1;
+		return caller.create(
+			  RSession[].class, CONTROLLER_NAME,
+			  new RSession[] {new RSession(toCreate)}, restTemplate
+		)[0].getId();
 	}
 	
 	@Override
 	public void delete(Session toDelete) {
-		//TODO
+		caller.delete(
+			  RSession[].class, CONTROLLER_NAME,
+			  new RSession[] {new RSession(toDelete)}, restTemplate
+		);
 	}
 	
 	@Override
 	public void delete(long idToDelete) {
-		//TODO
+		caller.deleteById(
+			  RSession.class, CONTROLLER_NAME,
+			  idToDelete, restTemplate
+		);
 	}
 	
 	@Override
 	public List<Session> getAll() {
-		restTemplate.getInterceptors().add(
-			  new BasicAuthorizationInterceptor("admin@nat.be", "adminadmin")
-		);
-		List<RSession> rSessions = caller.getAll(RSession[].class, CONTROLLER_NAME, restTemplate);
-		
 		List<Session> rtn = new ArrayList<>();
-		for (RSession ses : rSessions)
+		for (RSession ses : caller.getAll(RSession[].class, CONTROLLER_NAME, restTemplate))
 			rtn.add(ses.getModel());
 		return rtn;
 	}
 	
 	@Override
 	public Session getById(long id) {
-		restTemplate.getInterceptors().add(
-			  new BasicAuthorizationInterceptor("admin@nat.be", "adminadmin")
-		);
 		return caller.getById(RSession.class, CONTROLLER_NAME, id, restTemplate).getModel();
 	}
 	
 	@Override
 	public void update(Session toUpdate) {
-	
+		caller.update(
+			  RSession[].class, CONTROLLER_NAME,
+			  new RSession[] {new RSession(toUpdate)}, restTemplate);
 	}
 }
