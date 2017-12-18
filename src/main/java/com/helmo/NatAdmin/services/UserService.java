6d83a180e415d6fd09cd9ed9993688b9645954b5
Quentin Grignet <quentin.grignet@skynet.be>
2017-12-18 20:55:33 +0100

    Call REST Attributes

--- src/main/java/com/helmo/NatAdmin/services/UserService.java
+++ src/main/java/com/helmo/NatAdmin/services/UserService.java
@@ -2,7 +2,6 @@ package com.helmo.NatAdmin.services;
 
 import com.helmo.NatAdmin.caller.UserCall;
 import com.helmo.NatAdmin.models.User;
-import com.helmo.NatAdmin.caller.CallREST;
 import com.helmo.NatAdmin.reception.RUser;
 import com.helmo.NatAdmin.services.crudServices.ICrudService;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
@@ -26,13 +25,17 @@ public class UserService implements ICrudService<User> {
 		this.caller = userCall;
 	}
 	
+	private void setCredential(User user) {
+		restTemplate.getInterceptors().add(
+			  new BasicAuthorizationInterceptor(
+				    user.getEmail(),
+				    user.getPassword())
+		);
+	}
+	
 	@Override
 	public List<User> getAll(User cred) {
-		restTemplate.getInterceptors().add(
-				new BasicAuthorizationInterceptor(
-						cred.getEmail(),
-						cred.getPassword())
-		);
+		setCredential(cred);
 		
 		List<RUser> rUsers = caller.getAll(RUser[].class, CONTROLLER_NAME, restTemplate);
 		
@@ -44,31 +47,36 @@ public class UserService implements ICrudService<User> {
 	
 	@Override
 	public User getById(long id, User cred) {
-		
+		setCredential(cred);
 		return caller.getById(RUser.class, CONTROLLER_NAME, id, restTemplate).getModel();
 	}
 	
 	@Override
 	public void update(User toUpdate, User cred) {
+		setCredential(cred);
 		caller.update(RUser[].class, CONTROLLER_NAME, new RUser[] {new RUser(toUpdate)}, restTemplate);
 	}
 	
 	@Override
 	public long create(User toCreate, User cred) { //TODO Return the object itself
+		setCredential(cred);
 		return caller.create(RUser[].class, CONTROLLER_NAME, new RUser[] {new RUser(toCreate)}, restTemplate)[0].getId();
 	}
 	
 	@Override
 	public void delete(User toDelete, User cred) {
+		setCredential(cred);
 		caller.delete(RUser[].class, CONTROLLER_NAME, new RUser[] {new RUser(toDelete)}, restTemplate);
 	}
 	
 	@Override
 	public void delete(long idToDelete, User cred) {
+		setCredential(cred);
 		caller.deleteById(RUser.class, CONTROLLER_NAME, idToDelete, restTemplate);
 	}
 	
 	public User findByEmail(String email, User cred) {
+		setCredential(cred);
 		return caller.getByEmail(email, restTemplate).getModel();
 	}
 }
