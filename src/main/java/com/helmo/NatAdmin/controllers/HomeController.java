457616bf5286388ec435695ffbccc2c123ddbeca
Quentin Grignet <quentin.grignet@skynet.be>
2017-12-21 09:21:11 +0100

    It works

--- src/main/java/com/helmo/NatAdmin/controllers/HomeController.java
+++ src/main/java/com/helmo/NatAdmin/controllers/HomeController.java
@@ -1,8 +1,15 @@
 package com.helmo.NatAdmin.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
@@ -10,5 +17,14 @@ public class HomeController {
 	@Secured("ROLE_USER")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
 	}
 }
