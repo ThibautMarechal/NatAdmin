0323579904734243681a5af0e2dce1132b15352a
Thibaut Maréchal <contact@thibautmarechal.be>
2017-12-21 10:54:19 +0100

    trying to deploy

--- src/main/java/com/helmo/NatAdmin/NatAdminApplication.java
+++ src/main/java/com/helmo/NatAdmin/NatAdminApplication.java
@@ -1,13 +1,19 @@
 package com.helmo.NatAdmin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class NatAdminApplication extends SpringBootServletInitializer {
 	
